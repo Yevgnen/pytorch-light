cc140a562e7e6e65479b4cd516ddb45d98c5c704
Yevgnen Koh <wherejoystarts@gmail.com>
2021-06-29 22:07:56 +0800

    Add module utils
    Add ModeKeys

--- light/__init__.py
+++ light/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import light.masking
+import light.utils
 
 __version__ = "0.0.0"
--- setup.py
+++ setup.py
@@ -32,6 +32,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "torch",
+        "pytorch-ignite",
     ],
     test_suite="tests",
     zip_safe=False,
